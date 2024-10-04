4b324a3112377a6ac57ad1093ef117864b566258
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-05 01:05:20 +0600

    all the project is done

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ displayAllPhones = (phones) => {
   const phoneContainer = document.getElementById("phones-container");
   phones.forEach((phone) => {
     console.log(phone);
-    const {brand, image, slug, } = phone
+    const { brand, image, slug } = phone;
     const div = document.createElement("div");
     div.innerHTML = `
         <div class="card my-2 bg-base-100 w-96 shadow-xl">
@@ -58,8 +58,29 @@ const handleSearch = () => {
   }, 3000);
 };
 
-const phoneDetails = (slug) => {
-    console.log(slug)
-}
+const phoneDetails = async (slugs) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${slugs}`
+  );
+  const data = await response.json();
+  console.log(data.data);
+  const {brand, image, slug} = data.data
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.innerHTML = `
+    <dialog id="my_modal_1" class="modal">
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">${brand}</h3>
+    <p class="py-4">Press ESC key or click the button below to close</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+    `;
+    my_modal_1.showModal()
+};
 
 loadAllPhones(false, "iphone");
